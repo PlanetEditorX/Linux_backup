6855d45fc8c85ffdf8f11b1fe6ad9d5bcdf563c3
CoderX <85693093+PlanetEditorX@users.noreply.github.com>
2025-03-22 15:13:45 +0800

    add fnOS backup

--- armbian/backup.sh
+++ armbian/backup.sh
@@ -2,3 +2,6 @@
 
 # 备份系统
 dd if=/dev/mmcblk2 bs=4M status=progress | gzip > /mnt/backup/emmc_backup.img.gz
+
+# 1panel添加计划任务
+# bash /etc/soft/backup/backup.sh
--- armbian/recover.sh
+++ armbian/recover.sh
@@ -3,5 +3,3 @@
 # 恢复系统
 gzip -dc /mnt/backup/emmc_backup.img.gz | sudo dd of=/dev/mmcblk2 bs=4M status=progress conv=fsync
 
-# 1panel添加计划任务
-# bash /etc/soft/backup/backup.sh
